8bdf8b8fccb5fd732eaadd7d7d6689f1cb345f0b
Okeanij <dmitrij.polyanin@gmail.com>
2019-01-19 13:49:10 +0300

    +

--- build.sh
+++ build.sh
@@ -15,9 +15,17 @@ echo -e "\n$GREEN Publishing SunEngine $NC"
 dotnet publish -c Release SunEngine -o "$parent_path/Build"
 
 
+echo -e "\n$GREEN Clearing Images $NC"
+cd "$parent_path/Build/wwwroot/UploadImages/"
+shopt -s extglob
+rm -rf !(_)
+shopt -u extglob
+
+
 echo -e "$GREEN Building Client $NC"
-cd Client
+cd "$parent_path/Client"
 quasar build
+
 
 echo  -e "$GREEN Copying Client $NC"
 cp -rf "$parent_path/Client/dist/spa-mat/." "$parent_path/Build/wwwroot"
