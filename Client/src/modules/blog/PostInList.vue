b0e590d969b52a17489b818d588df692765e3ffd
Okeanij <dmitrij.polyanin@gmail.com>
2019-01-21 16:32:40 +0300

    Styles improvement

--- Client/src/modules/blog/PostInList.vue
+++ Client/src/modules/blog/PostInList.vue
@@ -6,21 +6,22 @@
         <div style="font-weight: 600">
           {{post.title}}
         </div>
-        <div class="q-my-xs info-block">
+        <div class="q-my-xs">
           <router-link :to="`/user/${post.authorLink}`" class="user-link q-mr-xl">
             {{post.authorName}}
           </router-link>
-          <span class="text-grey-6">
-            <q-icon name="far fa-clock"/>
-            {{$formatDate(this.post.publishDate)}} &nbsp;
-          </span>
+
         </div>
       </div>
     </q-item>
     <div class="post-preview" v-html="post.preview">
 
     </div>
-    <div class="flex footer">
+    <div class="date text-grey-6">
+      <q-icon name="far fa-clock"/>
+      <span>{{$formatDate(this.post.publishDate)}} &nbsp;</span>
+    </div>
+    <div class="flex footer float-left">
       <q-item :to="path+'#messages'" class="q-mr-md">
         <span :class="[{'text-grey-6': !post.messagesCount}]">
         <q-icon name="far fa-comment" class="q-mr-sm"/>
@@ -33,8 +34,9 @@
           <q-icon name="fas fa-arrow-right"/>
         </span>
       </q-item>
-    </div>
 
+    </div>
+    <div class="clear"></div>
   </div>
 </template>
 
@@ -77,15 +79,22 @@
     color: #3a67d3 !important;
   }
 
+  $footer-line-height = 38px;
+
   .footer {
     margin-left: -16px;
+    align-items: center;
 
     .q-item {
       color: #3a67d3 !important;
+      min-height: unset !important;
+    }
+
+    .q-item, div {
       margin-left: 2px;
-      padding-top: 6px !important;
-      padding-bottom: 6px !important;
-      min-height: 30px;
+      padding-top: unset !important;
+      padding-bottom: unset !important;
+      height: $footer-line-height;
     }
   }
 
@@ -111,8 +120,15 @@
     }
   }
 
-  .info-block span .q-icon {
-    margin-right: 7px;
+  .date {
+    display: flex;
+    float: right;
+    align-items: center;
+    height: $footer-line-height;
+
+    .q-icon {
+      margin-right: 7px;
+    }
   }
 
 </style>
