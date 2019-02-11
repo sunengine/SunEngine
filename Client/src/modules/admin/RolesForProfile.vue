330c97f76fafe96130218294a6f51a89a7002683
Okeanij <dmitrij.polyanin@gmail.com>
2019-02-11 14:16:56 +0300

    Styles and see roles of user on client start

---
+++ Client/src/modules/admin/RolesForProfile.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div v-if="userRoles">
+      Группы пользователя: <span v-for="role in userRoles" :class="'group-'+role.name.toLowerCase()">{{role.title}}</span>
+    </div>
+    <loader-wait v-else />
+  </div>
+</template>
+
+<script>
+  import LoaderWait from "../../components/LoaderWait";
+  export default {
+    name: "RolesForProfile",
+    components: {LoaderWait},
+    props: {
+      userId: {
+        type: Number,
+        required: true
+      }
+    },
+    data: function() {
+      return {
+        allRoles: null,
+        userRoles: null,
+        availableRoles: null
+      }
+    },
+    methods: {
+      async loadData() {
+
+      }
+    },
+    async created() {
+      await this.loadData();
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- SunEngine/Models/Authorization/UserGroup.cs
+++ SunEngine/Models/Authorization/UserGroup.cs
@@ -6,7 +6,7 @@ namespace SunEngine.Models.Authorization
     public class UserGroup : IdentityRole<int>
     {
         public string Title { get; set; }
-
+        
         public int SortNumber { get; set; }
 
         public bool IsSuper { get; set; }
