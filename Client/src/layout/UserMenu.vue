3c5a452ddb86c070f96622799f7562ebf58a9c62
Okeanij <dmitrij.polyanin@gmail.com>
2019-03-02 10:06:36 +0300

    +

--- Client/src/layout/UserMenu.vue
+++ Client/src/layout/UserMenu.vue
@@ -1,30 +1,29 @@
-
 <template>
-  <div>
-    <q-list class="menu-list">
-      <q-item :multiline="false">
-        <img class="avatar" :src="userInfo.avatar"/>
-        <!--<q-icon name="fas fa-user" />-->
-        {{user.name}}
-      </q-item>
-      <q-item :to="$buildPath(`/user/${userInfo.link}`)" >
-        <q-icon name="fas fa-user-circle" />
-        Профиль
-      </q-item>
-      <q-item to="/personal" >
-        <q-icon name="fas fa-address-card" />
-        Личный кабинет
-      </q-item>
-      <q-item v-if="isAdmin" :to="{name: 'Admin'}">
-        <q-icon name="fas fa-cog" />
-        Админка
-      </q-item>
-      <q-item v-close-overlay @click.native="logout()" link>
-        <q-icon name="fas fa-sign-out-alt" />
-        Выйти
-      </q-item>
-    </q-list>
-  </div>
+
+  <q-list class="menu-list">
+    <q-item :multiline="false">
+      <img class="avatar" :src="userInfo.avatar"/>
+      <!--<q-icon name="fas fa-user" />-->
+      {{user.name}}
+    </q-item>
+    <q-item :to="$buildPath(`/user/${userInfo.link}`)">
+      <q-icon name="fas fa-user-circle"/>
+      Профиль
+    </q-item>
+    <q-item to="/personal">
+      <q-icon name="fas fa-address-card"/>
+      Личный кабинет
+    </q-item>
+    <q-item v-if="isAdmin" :to="{name: 'Admin'}">
+      <q-icon name="fas fa-cog"/>
+      Админка
+    </q-item>
+    <q-item v-close-overlay @click.native="logout()" link>
+      <q-icon name="fas fa-sign-out-alt"/>
+      Выйти
+    </q-item>
+  </q-list>
+
 </template>
 
 <script>
@@ -46,19 +45,19 @@
       logout() {
         this.$store.dispatch('request', {url: '/Account/Logout'})
           .then(x => {
-          this.$q.notify({
-            message: `Вы вышли`,
-            timeout: 2000,
-            type: 'info',
-            position: 'top'
+            this.$q.notify({
+              message: `Вы вышли`,
+              timeout: 2000,
+              type: 'info',
+              position: 'top'
+            });
           });
-        });
       }
     }
   }
 </script>
 
-<style lang="stylus" scoped >
+<style lang="stylus" scoped>
   .avatar {
     margin-left: -6px;
     margin-right: 11px;
