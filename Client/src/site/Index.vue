d6fb98409c7d2e42e27534c05892f7046136cfbd
Dmitriy Polianin <dmitrij.polyanin@gmail.com>
2019-03-19 08:30:15 +0300

    version +

--- Client/src/site/Index.vue
+++ Client/src/site/Index.vue
@@ -10,7 +10,7 @@
       <p>При регистрации можете указывать любой почтовый ящик, например <span class="hl">ooo@ooo.eml</span>.
         <br/>
         Почта будет отправляться на <a target="_blank" href="http://demo.sunengine.site:1444"><b>mailcatcher</b></a>.</p>
-      <p class="ver q-mb-xl">Версия: 0.11.3</p>
+      <p class="ver q-mb-xl">Версия: 0.11.4</p>
 
     </div>
   </q-page>
