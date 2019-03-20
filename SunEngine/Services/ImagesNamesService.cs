23aab606ccd4e086b01b469087a860628af0299d
Dmitriy Polianin <dmitrij.polyanin@gmail.com>
2019-03-20 14:15:50 +0300

    Removed not needed code

--- SunEngine/Services/ImagesNamesService.cs
+++ SunEngine/Services/ImagesNamesService.cs
@@ -30,41 +30,16 @@ namespace SunEngine.Services
 
     public class ImagesNamesService : IImagesNamesService
     {
-        /*private readonly string basePath;
-        
-        public ImagesNamesService(IHostingEnvironment env, IOptions<ImagesOptions> imagesOptions)
-        {
-            basePath = Path.Combine(env.WebRootPath, imagesOptions.Value.UploadDir);
-        }*/
-
         public FileAndDir GetNewImageNameAndDir(string ext)
         {
-            while (true)
-            {
-                //var guid = Guid.NewGuid();
-                //var bites = guid.ToByteArray();
-                //return new FileAndDir(guid.ToString("N") + ext, bites[0].ToString());
+            var cid = CryptoRandomizer.GetRandomString(
+                DbColumnSizes.FileNameWithDirSize -
+                8); // Why -8, 4 needed for directory start ("123/"), and 4 needed for extension (".jpg")
+            byte[] bites = Encoding.UTF8.GetBytes(cid);
 
-                var cid = CryptoRandomizer.GetRandomString(
-                    DbColumnSizes.FileNameWithDirSize -
-                    8); // Why -8, 4 needed for directory start ("123/"), and 4 needed for extension (".jpg")
-                byte[] bites = Encoding.UTF8.GetBytes(cid);
-
-                return new FileAndDir(cid + ext, bites[0].ToString());
-
-                /*
-                 FileAndDir rez = new FileAndDir(cid + ext, bites[0].ToString());
-                 
-                 if (!CheckFileOnDisk(rez.Path))
-                    return rez;
-                return rez;*/
-            }
+            return new FileAndDir(cid + ext, bites[0].ToString());
         }
-
-        /*private bool CheckFileOnDisk(string path)
-        {
-            var fullPath = Path.Combine(basePath, path);
-            return File.Exists(fullPath);
-        }*/
     }
+}
+
 }
