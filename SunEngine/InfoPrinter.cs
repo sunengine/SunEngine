72308081a9ee8e733683d86507976facf14f3797
Dmitriy Polianin <dmitrij.polyanin@gmail.com>
2019-04-26 20:01:49 +0300

    Make no arguments to run server is Dev:SunEngineDllRunServer Otherwise show info

--- SunEngine/InfoPrinter.cs
+++ SunEngine/InfoPrinter.cs
@@ -7,17 +7,26 @@ namespace SunEngine
 {
     public static class InfoPrinter
     { 
+        /// <summary>
+        /// Print this info if "dotnet SunEngine.dll" starts with no commands.
+        /// </summary>
         public static void PrintVoidStartInfo()
         {
             Console.WriteLine("You start \"SunEngine.dll\" with out arguments.\nRun with \"help\" argument to list commands:\n\tdotnet SunEngine.dll help");
         }
         
+        /// <summary>
+        /// Print help on dotnet "dotnet SunEngine.dll help"
+        /// </summary>
         public static void PrintHelp()
         {
             var text = File.ReadAllText(Path.GetFullPath("Resources/help.txt"));
             Console.WriteLine(text);
         }
 
+        /// <summary>
+        /// Print version of SunEngine
+        /// </summary>
         public static void PrintVersion()
         {
             Assembly assembly = Assembly.GetExecutingAssembly();
