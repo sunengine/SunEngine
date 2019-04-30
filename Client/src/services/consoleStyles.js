36e7d388cb624eb0128e84757e70af327975f9c2
kreuwont <icdg@yandex.ru>
2019-05-01 00:18:44 +0300

    Remove file

--- Client/src/services/consoleStyles.js
+++
@@ -1,8 +0,0 @@
-
-export var consoleInit = "color:white; background-color: #49AB00; border-radius: 5px; padding: 1px 7px;";
-export var consoleUserLogout = "color:white; background-color: #EE7063; border-radius: 5px; padding: 1px 7px;";
-
-export var consoleRequestStart = "color: darkgreen; background: #D6FF90; border-radius: 5px 0 0 5px; padding: 1px 7px;";
-export var consoleRequestUrl = "color: darkgreen; background: #FFF2C7; border-radius: 0 5px 5px 0; padding: 1px 7px;";
-
-export var consoleTokens = "color:white; background-color: #D7AE37; border-radius: 5px; padding: 1px 7px;";
