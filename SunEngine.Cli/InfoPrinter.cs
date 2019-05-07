7b175594b95add842de6ff1881e821b39506ec47
Dmitriy Polianin <dmitrij.polyanin@gmail.com>
2019-05-07 11:20:16 +0300

    Updated 'help' command text

--- SunEngine.Cli/InfoPrinter.cs
+++ SunEngine.Cli/InfoPrinter.cs
@@ -25,14 +25,16 @@ Commands:
     config:<path>               path to config directory, if none 'Config' is default 
     migrate                     make initial database table structure and migrations in existing database
     init                        initialize users, roles and categories tables from config directory
+    test-dbcon                  check is data base connection is working                     
     version                     print SunEngine version
     help                        show this help   
     
 Seed test data commands:    
-    seed:<CatName>:<X>:<Y>      seed category and all subcategories with materials and comments
-                                CatName - category name, 'Root' if skipped
-                                X - materials count, default if skipped
-                                Y - comments count, default if skipped
+    seed:<CategoryName>:<MCount>:<CCount>      
+                                seed category and all subcategories with materials and comments
+                                MCount - materials count, default if skipped
+                                CCount - comments count, default if skipped
+                                example - seed:SomeCategory:20:10
                                 
     append-cat-name             add category name to material titles on 'seed'
 
@@ -40,6 +42,7 @@ Examples:
     dotnet SunEngine.dll server
     dotnet SunEngine.dll server config:local.Config.MySite
     dotnet SunEngine.dll migrate init seed
+    dotnet SunEngine.dll migrate init seed config:local.Config.MySite
     dotnet SunEngine.dll seed:Forum:10:10
 ";
             Console.WriteLine(helpText);
