34cd769aa8fa61e1f5bf8898b34006a031165864
LinqManJr <johnybond32@yandex.ru>
2019-05-18 23:49:03 +0900

    add SunEngine.Admin tests

---
+++ SunEngine.Tests/SunEngine.Admin/Managers/CategoriesAdminManagerTests.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Linq;
+using SunEngine.Admin.Managers;
+using SunEngine.Core.Cache.Services;
+using SunEngine.Core.DataBase;
+using SunEngine.Core.Models;
+using SunEngine.Core.Services;
+using SunEngine.Core.Utils.TextProcess;
+using Xunit;
+
+namespace SunEngine.Tests.SunEngine.Admin.Managers
+{
+    public class CategoriesAdminManagerTests
+    {
+        private readonly CategoriesAdminManager catAdminManager;
+        private DataBaseConnection dbConnection;
+
+        public CategoriesAdminManagerTests()
+        {
+            catAdminManager = DefaultCatAdminManager();
+        }
+
+        private CategoriesAdminManager DefaultCatAdminManager()
+        {
+            dbConnection = DefaultInit.GetTestDataBaseConnection();
+            var dbFactory = DefaultInit.GetTestDataBaseFactory();
+            var catCache = new CategoriesCache(dbFactory);
+
+            return new CategoriesAdminManager(dbConnection, catCache, new Sanitizer());
+        }
+
+        private Category DefaultCategory => new Category { Name = "TestCategory", ParentId = 1, Title= "TestCategoryTitle"};
+
+        #region Test CreateCategoryAsync
+
+        [Fact]
+        public async void ShouldCreateCategory()
+        {
+            var category = DefaultCategory;
+
+            using (var transaction = dbConnection.BeginTransaction())
+            {
+                int countBefore = dbConnection.Categories.Count();
+                await catAdminManager.CreateCategoryAsync(category);
+                int countAfter = dbConnection.Categories.Count();
+                transaction.Rollback();
+                Assert.NotEqual(countAfter, countBefore);
+            }
+        }
+
+        [Fact]
+        public async void ShouldThrowExceptionIfCategoryIsNullWhenCreate()
+        {
+            await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+                await catAdminManager.CreateCategoryAsync(null));
+        }
+
+        [Fact]
+        public async void ShouldThrowExceptionIfParentIdIsNull()
+        {
+            var category = new Category {Name = "Test", Title ="test"};
+            await Assert.ThrowsAsync<ParentCategoryNotFoundByIdException>(async () =>
+                await catAdminManager.CreateCategoryAsync(category));
+        }
+
+        [Fact]
+        public async void ShouldThrowExceptionIfCategoryNameIsNull()
+        {
+            var category = new Category { Title = "test" };
+            await Assert.ThrowsAsync<InvalidModelException>(async () =>
+                await catAdminManager.CreateCategoryAsync(category));
+        }
+
+        [Fact]
+        public async void ShouldThrowExceptionIfCategoryTitleIsNull()
+        {
+            var category = new Category { Name = "Test"};
+            await Assert.ThrowsAsync<InvalidModelException>(async () =>
+                await catAdminManager.CreateCategoryAsync(category));
+        }
+
+        #endregion
+
+        #region Test UpdateCategoryAsync
+
+        [Fact]
+        public async void ShouldUpdateCategory()
+        {
+            var category = DefaultCategory;
+
+            using (var transaction = dbConnection.BeginTransaction())
+            {
+                await catAdminManager.CreateCategoryAsync(category);
+                category = dbConnection.Categories.FirstOrDefault(x => x.Name == category.Name);
+                Assert.NotNull(category);
+                category.Name = "UpdatedTestCategory";
+                await catAdminManager.UpdateCategoryAsync(category);
+                var resultCategory = dbConnection.Categories.FirstOrDefault(x => x.Name == "UpdatedTestCategory");
+                Assert.NotNull(resultCategory);
+                transaction.Rollback();
+            }
+        }
+
+        [Fact]
+        public async void ShouldThrowExceptionWhenUpdateCategoryWithNull()
+        {
+            await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+                await catAdminManager.UpdateCategoryAsync(null));
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenUpdateCategoryWithWrongId()
+        {
+            var category = DefaultCategory;
+            category.Id = -1;
+            var ex = Record.ExceptionAsync(async () => await catAdminManager.UpdateCategoryAsync(category));
+            Assert.Equal($"No category with {category.Id} id", ex.Result.Message);
+        }
+
+        [Fact]
+        public async void ShouldThrowExceptionWhenUpdateCategoryWithWrongParentId()
+        {
+            var category = DefaultCategory;
+
+            using (var transaction = dbConnection.BeginTransaction())
+            {
+                await catAdminManager.CreateCategoryAsync(category);
+                category = dbConnection.Categories.FirstOrDefault(x => x.Name == category.Name);
+                Assert.NotNull(category);
+                category.ParentId = -1;
+                await Assert.ThrowsAsync<ParentCategoryNotFoundByIdException>(async () =>
+                    await catAdminManager.UpdateCategoryAsync(category));
+                transaction.Rollback();
+            }
+        }
+
+
+        #endregion
+
+        #region Test CategoryUp
+
+        [Fact]
+        public async void ShouldMoveCategoryUp()
+        {
+            var category = DefaultCategory;
+            
+            using (var transaction = dbConnection.BeginTransaction())
+            {
+                await catAdminManager.CreateCategoryAsync(category);
+                var result = await catAdminManager.CategoryUp("TestCategory");
+                transaction.Rollback();
+                Assert.Equal(ServiceResult.OkResult().Error, result.Error);
+                Assert.Equal(ServiceResult.OkResult().Failed, result.Failed);
+                Assert.Equal(ServiceResult.OkResult().Succeeded, result.Succeeded);
+            }
+        }
+
+        [Theory]
+        [InlineData("fakeCategory")]
+        [InlineData("Root")]
+        public async void ShouldReturnBadResultWhenCategoryUp(string name)
+        {
+            var result = await catAdminManager.CategoryUp(name);
+            Assert.Equal(ServiceResult.BadResult().Failed, result.Failed);
+        }
+
+        #endregion
+
+        #region Test CategoryDown
+
+
+        [Fact]
+        public async void ShouldMoveCategoryDown()
+        {
+            using (var transaction = dbConnection.BeginTransaction())
+            {
+                var result = await catAdminManager.CategoryDown("Articles");
+                transaction.Rollback();
+                Assert.Equal(ServiceResult.OkResult().Succeeded, result.Succeeded);
+            }
+        }
+
+        [Theory]
+        [InlineData("fakeCategory")]
+        public async void ShouldReturnBadResultWhenCategoryDown(string name)
+        {
+            var result = await catAdminManager.CategoryUp(name);
+            Assert.Equal(ServiceResult.BadResult().Failed, result.Failed);
+        }
+
+        #endregion
+
+        #region Test CategoryMoveToTrash
+        
+        [Fact]
+        public async void ShouldMoveCategoryToTrash()
+        {
+            var category = DefaultCategory;
+            using (var transaction = dbConnection.BeginTransaction())
+            {
+                await catAdminManager.CreateCategoryAsync(category);
+                await catAdminManager.CategoryMoveToTrashAsync(category.Name);
+                var result = dbConnection.Categories.FirstOrDefault(x =>x.Name == category.Name);
+                Assert.NotNull(result);
+                Assert.True(result.IsDeleted);
+                transaction.Rollback();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ SunEngine.Tests/SunEngine.Admin/Services/RolesPermissionsAdminServiceTests.cs
@@ -0,0 +1,64 @@
+using System.IO;
+using System.Linq;
+using System.Text;
+using LinqToDB;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using SunEngine.Admin.Services;
+using SunEngine.Core.DataBase;
+using Xunit;
+
+namespace SunEngine.Tests.SunEngine.Admin.Services
+{
+    public class RolesPermissionsAdminServiceTests
+    {
+        private readonly IHostingEnvironment environment;
+        private readonly DataBaseConnection dbConnection;
+
+        public RolesPermissionsAdminServiceTests()
+        {
+            dbConnection = DefaultInit.GetTestDataBaseConnection();
+            environment = new HostingEnvironment {ContentRootPath = GetUserGroupSchemaPath()};
+        }
+        
+
+        private string GetUserGroupSchemaPath() => Path.Combine(GetParentDirectoryAtLevel(Directory.GetCurrentDirectory(), 4), "SunEngine.Cli");
+        private string GetUserGroupJsonPath() => Path.Combine(GetParentDirectoryAtLevel(Directory.GetCurrentDirectory(), 4), "SunEngine.Cli", "Config", "UserGroups.json");
+
+        private string GetParentDirectoryAtLevel(string path, int level)
+        {
+            while (level != 0)
+            {
+                var newPath = Directory.GetParent(path).FullName;
+                return GetParentDirectoryAtLevel(newPath, --level);
+            }
+
+            return path;
+        }
+
+        [Fact]
+        public async void ShouldGetGroupJson()
+        {
+            var _rolesService = new RolesPermissionsAdminService(dbConnection, environment);
+            Assert.NotEqual(string.Empty, await _rolesService.GetGroupsJsonAsync());
+        }
+
+        
+        [Fact]
+        public async void ShouldLoadUserGroupsFromJson()
+        {
+            string json = File.ReadAllText(GetUserGroupJsonPath(), Encoding.Default);
+            var rolesService = new RolesPermissionsAdminService(dbConnection, environment);
+
+            using (var transaction = dbConnection.BeginTransaction())
+            {
+                await dbConnection.Roles.Set(x => x.Name, "test").UpdateAsync();
+                var before = dbConnection.Roles.Select(x => x.Name).ToList();
+                await rolesService.LoadUserGroupsFromJsonAsync(json);
+                var after = dbConnection.Roles.Select(x => x.Name).ToList();
+                Assert.NotEqual(before, after);
+                transaction.Rollback();
+            }  
+        }
+    }
+}
