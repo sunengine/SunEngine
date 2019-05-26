ce1079cb172c5eeb0cbe2b4e79ec1938d08a2907
Pavel Erokhin <MairwunNx@gmail.com>
2019-05-26 18:51:31 +0300

    Added tests for MailTemplatesCache class.

--- SunEngine.Tests/MailTemplatesCache.cs
+++ SunEngine.Tests/MailTemplatesCache.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Diagnostics;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -9,21 +8,20 @@ namespace SunEngine.Tests
     {
         private readonly ITestOutputHelper testOutputHelper;
 
+        public MailTemplatesCache(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
         private readonly Dictionary<string, string> exceptedDictionary =
             new Dictionary<string, string>();
 
         private readonly Core.Cache.Services.MailTemplatesCache mailTemplatesCache =
             new Core.Cache.Services.MailTemplatesCache();
 
-        public MailTemplatesCache(ITestOutputHelper testOutputHelper)
-        {
-            this.testOutputHelper = testOutputHelper;
-        }
-
         [Fact]
-        internal void FileDictionaryExistsTest()
+        internal void FileContentEqualExistsTest()
         {
-            mailTemplatesCache.Initialize();
             exceptedDictionary.Add("test-template.html", "<head></head>\n");
             Assert.Equal(
                 exceptedDictionary.ToString(),
@@ -31,25 +29,28 @@ namespace SunEngine.Tests
             );
         }
 
-//        [Fact]
-//        internal void KeyValueExistsTest()
-//        {
-//            mailTemplatesCache.Initialize();
-//            Assert.Equal(
-//                "<head></head>\r\n",
-//                mailTemplatesCache.BuildMessage(
-//                    "test-template.html",
-//                    null
-//                )
-//            );
-//        }
         [Fact]
-        internal void ParseStringTest()
+        internal void BuildMessageSubjectTest()
         {
-            var s = new Dictionary<string, string> {{"link", "https://google.com/"}};
-            var ss = mailTemplatesCache.BuildMessage("register.html", s);
-            Debug.WriteLine(ss.subject);
-            Debug.WriteLine(ss.template);
+            Assert.Equal(
+                "Please confirm your account",
+                mailTemplatesCache.BuildMessage(
+                    "test-register.html",
+                    new Dictionary<string, string> {{"space", ""}}
+                ).subject
+            );
+        }
+
+        [Fact]
+        internal void BuildMessageBodyTest()
+        {
+            Assert.Equal(
+                "Please confirm your account by clicking this <a href=\"google.com\">link</a>.",
+                mailTemplatesCache.BuildMessage(
+                    "test-register.html",
+                    new Dictionary<string, string> {{"[link]", "google.com"}}
+                ).template
+            );
         }
     }
 }
