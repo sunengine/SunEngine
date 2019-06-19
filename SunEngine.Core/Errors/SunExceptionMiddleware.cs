8ef2ace3ee6f029cd078a604c205d8ea4d1cfa6c
Dmitriy Polianin <dmitrij.polyanin@gmail.com>
2019-06-19 22:51:01 +0300

    Add log to console to SunExceptionMiddleware

--- SunEngine.Core/Errors/SunExceptionMiddleware.cs
+++ SunEngine.Core/Errors/SunExceptionMiddleware.cs
@@ -8,29 +8,35 @@ namespace SunEngine.Core.Errors
 {
     public class SunExceptionMiddleware
     {
-        private readonly RequestDelegate _next;
-        //private readonly ILogger<SunExceptionMiddleware> _logger;
+        private readonly RequestDelegate next;
+        private readonly ILogger<SunExceptionMiddleware> logger;
 
         public SunExceptionMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
         {
-            _next = next ?? throw new ArgumentNullException(nameof(next));
-            //_logger = loggerFactory?.CreateLogger<SunExceptionMiddleware>() ?? throw new ArgumentNullException(nameof(loggerFactory));
+            this.next = next ?? throw new ArgumentNullException(nameof(next));
+            logger = loggerFactory?.CreateLogger<SunExceptionMiddleware>() ?? throw new ArgumentNullException(nameof(loggerFactory));
         }
 
         public async Task Invoke(HttpContext context)
         {
             try
             {
-                await _next(context);
+                await next(context);
             }
             catch (SunViewException e)
             {
                 await context.Response.WriteAsync(SunJson.Serialize(e.ErrorView));
+                
+                context.Response.StatusCode = 500;
+                logger.LogError(e.ToString());
             }
             catch (Exception e)
             {
                 ErrorView errorView = ErrorView.ServerError(e);
                 await context.Response.WriteAsync(SunJson.Serialize(errorView));
+                
+                context.Response.StatusCode = 500;
+                logger.LogError(e.ToString());
             }
         }
     }
