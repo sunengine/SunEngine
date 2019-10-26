e85108de18e74e4dff1460fedcac2cb927979ab9
Okeanij <dmitrtij.polyanin@gmail.com>
2019-10-26 21:44:13 +0300

    Add BEM to SendPrivateMessage.vue

--- Client/src/modules/profile/SendPrivateMessage.vue
+++ Client/src/modules/profile/SendPrivateMessage.vue
@@ -1,17 +1,19 @@
 <template>
   <q-page class="send-private-message page-padding">
-    <h2 class="q-title"> {{$tl("titleStart")}}
+    <h2 class="header-title"> {{$tl("titleStart")}}
       <q-icon name="far fa-user" color="grey-7"/>
       {{userName}}
     </h2>
 
-    <q-editor class="q-mb-md" :toolbar="editorToolbar" ref="htmlEditor" v-model="text"/>
+    <q-editor class="send-private-message__editor q-mb-md" :toolbar="editorToolbar" ref="htmlEditor" v-model="text"/>
 
-    <q-btn no-caps icon="fas fa-arrow-circle-right" class="send-btn q-mr-sm" @click="send" :loading="loading"
+    <q-btn no-caps icon="fas fa-arrow-circle-right" class="send-private-message__send-btn send-btn q-mr-sm"
+           @click="send" :loading="loading"
            :label="$tl('sendBtn')">
       <loader-sent slot="loading"/>
     </q-btn>
-    <q-btn no-caps icon="fas fa-times" @click="$router.back()" class="cancel-btn" :label="$t('Global.btn.cancel')"/>
+    <q-btn no-caps icon="fas fa-times" @click="$router.back()" class="send-private-message__cancel-btn cancel-btn"
+           :label="$t('Global.btn.cancel')"/>
   </q-page>
 </template>
 
@@ -49,13 +51,13 @@
                     {
                         userId: this.userId,
                         text: this.text
-                    })
-                    .then(() => {
-                            this.$successNotify();
-                            this.loading = false;
-                            this.$router.back();
-                        }
-                    ).catch(error => {
+                    }
+                ).then(_ => {
+                        this.$successNotify();
+                        this.loading = false;
+                        this.$router.back();
+                    }
+                ).catch(error => {
                     this.$errorNotify(error);
                     this.loading = false;
                 })
