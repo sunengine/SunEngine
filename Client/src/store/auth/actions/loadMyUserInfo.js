6e9f365e8abde8ae4d4f5e932ef270d3768d1ccb
Okeanij <dmitrtij.polyanin@gmail.com>
2019-11-12 12:47:59 +0300

    +

--- Client/src/store/auth/actions/loadMyUserInfo.js
+++ Client/src/store/auth/actions/loadMyUserInfo.js
@@ -1,16 +1,20 @@
 import {consoleTokens, removeTokens, request} from 'sun'
 import {Api} from 'sun'
+import {showErrorsNotify} from 'sun'
 
 export default function (context, data) {
+
 
   return request(
     Api.Personal.GetMyUserInfo,
     {
-      skipLock: data?.skipLock
+      skipLock: data?.skipLock,
+      showErrorsNotifications: false,
+      blockErrorsNotifications: true
     }).then(response => {
     context.commit('setUserInfo', response.data);
   }).catch(() => {
     console.error('%cTokens removed', consoleTokens);
     removeTokens();
-  });
+  })
 }
