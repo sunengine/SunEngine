c0de119e3eafd578f9fda258df4af5a587b23d4e
Ilya <icdg@yandex.ru>
2019-12-12 20:24:36 +0300

    Refactor spam protection + add new functions

--- Server/SunEngine.Core/Filters/IpSpamProtectionFilter.cs
+++ Server/SunEngine.Core/Filters/IpSpamProtectionFilter.cs
@@ -9,67 +9,18 @@ using SunEngine.Core.Errors;
 
 namespace SunEngine.Core.Filters
 {
-    public class IpSpamProtectionFilter : ActionFilterAttribute
+    public class IpSpamProtectionFilter : SpamProtectionFilter
     {
-        private const string CacheKeyStart = "RFIP";
+      protected override string CacheKey => "RFIP";
 
-        public int TimeoutSeconds
-        {
-            set => timeout = TimeSpan.FromSeconds(value);
-            get => (int) timeout.TotalSeconds;
-        }
+      protected override string GetClientId(BaseController controller)
+      {
+        return controller.Request.HttpContext.Connection.RemoteIpAddress.ToString();
+      }
 
-        protected TimeSpan timeout;
-
-
-        public override void OnActionExecuting(ActionExecutingContext context)
-        {
-            SpamProtectionCache spamProtectionCache =
-                context.HttpContext.RequestServices.GetRequiredService<SpamProtectionCache>();
-
-            BaseController controller = (BaseController) context.Controller;
-
-            var actionDescriptor = context.ActionDescriptor as ControllerActionDescriptor;
-            string controllerName = actionDescriptor?.ControllerTypeInfo.FullName;
-            string actionName = actionDescriptor?.ActionName;
-
-            var ip = controller.Request.HttpContext.Connection.RemoteIpAddress;
-
-            string key = MakeKey(ip, controllerName, actionName);
-            RequestFree requestFree = spamProtectionCache.Find(key);
-
-            if (requestFree != null && requestFree.Working())
-                context.Result =
-                    controller.BadRequest(new Error("SpamProtection", "To rapid requests does not allowed",ErrorType.Soft));
-
-            SpamProtectionFilterTransfer temp = new SpamProtectionFilterTransfer
-            {
-                Key = key,
-                RequestFree = requestFree,
-                SpamProtectionCache = spamProtectionCache
-            };
-
-            controller.ViewData[SpamProtectionFilterTransfer.ViewDataKey] = temp;
-        }
-
-        private static string MakeKey(IPAddress ip, string controllerName, string actionName)
-        {
-            return $"{CacheKeyStart}-{ip}-{controllerName}-{actionName}";
-        }
-
-        public override void OnResultExecuted(ResultExecutedContext context)
-        {
-            if (context.HttpContext.Response.StatusCode != 200)
-                return;
-
-            BaseController controller = (BaseController) context.Controller;
-
-            var temp = (SpamProtectionFilterTransfer) controller.ViewData[SpamProtectionFilterTransfer.ViewDataKey];
-
-            if (temp.RequestFree != null)
-                temp.RequestFree.UpdateDateTime(timeout);
-            else
-                temp.SpamProtectionCache.Add(temp.Key, new RequestFree(timeout));
-        }
+      protected override TimeSpan CalcTimeout(BaseController controller)
+      {
+        return blockTimeout;
+      }
     }
 }
--- Server/SunEngine.Core/Filters/UserSpamProtectionFilter.cs
+++ Server/SunEngine.Core/Filters/UserSpamProtectionFilter.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,108 +10,28 @@ using SunEngine.Core.Security;
 
 namespace SunEngine.Core.Filters
 {
-    public class UserSpamProtectionFilter : ActionFilterAttribute
+  public class UserSpamProtectionFilter : SpamProtectionFilter
+  {
+    private readonly TimeSpan AdminTimeout = new TimeSpan(0, 0, 5);
+    protected override string CacheKey => "RFUSER";
+
+    public override void OnActionExecuting(ActionExecutingContext context)
     {
-        private const string CacheKeyStart = "RFUSER";
-        private readonly TimeSpan AdminTimeout = new TimeSpan(0, 0, 5);
+      var user = context.HttpContext.User;
+      if (!user.Identity.IsAuthenticated)
+        context.Result = (context.Controller as BaseController)?.Unauthorized();
 
-
-        public int TimeoutSeconds
-        {
-            set => timeout = TimeSpan.FromSeconds(value);
-            get => (int) timeout.TotalSeconds;
-        }
-
-        protected TimeSpan timeout;
-
-
-        public override void OnActionExecuting(ActionExecutingContext context)
-        {
-            SpamProtectionCache spamProtectionCache =
-                context.HttpContext.RequestServices.GetRequiredService<SpamProtectionCache>();
-
-            BaseController controller = (BaseController) context.Controller;
-
-            var user = controller.User;
-
-            if (!user.Identity.IsAuthenticated)
-                context.Result = controller.Unauthorized();
-
-            var actionDescriptor = context.ActionDescriptor as ControllerActionDescriptor;
-            string controllerName = actionDescriptor?.ControllerTypeInfo.FullName;
-            string actionName = actionDescriptor?.ActionName;
-
-            string key = MakeKey(user.UserId, controllerName, actionName);
-            RequestFree requestFree = spamProtectionCache.Find(key);
-
-
-            if (requestFree != null && requestFree.Working())
-                context.Result =
-                    controller.BadRequest(new Error("SpamProtection", "To rapid requests does not allowed", ErrorType.Soft));
-
-            SpamProtectionFilterTransfer temp = new SpamProtectionFilterTransfer
-            {
-                Key = key,
-                RequestFree = requestFree,
-                SpamProtectionCache = spamProtectionCache
-            };
-
-            controller.ViewData[SpamProtectionFilterTransfer.ViewDataKey] = temp;
-        }
-
-        private static string MakeKey(int userId, string controllerName, string actionName)
-        {
-            return string.Join("-", CacheKeyStart, userId, controllerName, actionName);
-        }
-
-        public override void OnResultExecuted(ResultExecutedContext context)
-        {
-            if (context.HttpContext.Response.StatusCode != 200)
-                return;
-
-            BaseController controller = (BaseController) context.Controller;
-
-            var temp = (SpamProtectionFilterTransfer) controller.ViewData[SpamProtectionFilterTransfer.ViewDataKey];
-
-            var thisTimeout = controller.User.IsInRole(RoleNames.Admin) ? AdminTimeout : timeout;
-
-
-            if (temp.RequestFree != null)
-                temp.RequestFree.UpdateDateTime(thisTimeout);
-            else
-                temp.SpamProtectionCache.Add(temp.Key, new RequestFree(thisTimeout));
-        }
+      base.OnActionExecuting(context);
     }
 
-    /// <summary>
-    /// Object to transfer data between OnActionExecuting and OnResultExecuted
-    /// </summary>
-    public class SpamProtectionFilterTransfer
+    protected override string GetClientId(BaseController controller)
     {
-        public const string ViewDataKey = "SpamProtectionFilterTransfer";
-
-        public string Key;
-        public RequestFree RequestFree;
-        public SpamProtectionCache SpamProtectionCache;
+      return controller.User.UserId.ToString();
     }
 
-    public class RequestFree
+    protected override TimeSpan CalcTimeout(BaseController controller)
     {
-        private DateTime dateTimeTil;
-
-        public RequestFree(TimeSpan timeout)
-        {
-            dateTimeTil = DateTime.UtcNow.Add(timeout);
-        }
-
-        public void UpdateDateTime(TimeSpan timeout)
-        {
-            dateTimeTil = DateTime.UtcNow.Add(timeout);
-        }
-
-        public bool Working()
-        {
-            return dateTimeTil >= DateTime.UtcNow;
-        }
+      return controller.User.IsInRole(RoleNames.Admin) ? AdminTimeout : blockTimeout;
     }
+  }
 }
