5d1e9bcb29f6385980392a1f60995d75cf12f156
Okeanij <dmitrtij.polyanin@gmail.com>
2019-12-15 16:29:26 +0300

    +

---
+++ Client/src/site.template/i18n/ru.js
@@ -0,0 +1,27 @@
+export default {
+  IndexPage: {
+    users: "Пользователи",
+    password: "Пароль",
+    version: "Версия"
+  },
+  MainMenu: {
+    Home: "Домой",
+    News: "Новости",
+    Documentation: "Документация",
+    Forum: "Форум",
+    demo: "Демо"
+  },
+  Layout: {
+    madeWithLove: "Сделано с Любовью",
+  },
+  MaterialInlinePage: {
+    title: "Страница со встроенной статьёй"
+  },
+  News2ColPage: {
+    title: "Главная страница",
+    subTitle: "Пример"
+  },
+  SecretPage: {
+    title: "Секретная страница"
+  }
+}
