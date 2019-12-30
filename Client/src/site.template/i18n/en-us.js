0da300b2e3421010117974bc926596113a1a2dbb
Okeanij <dmitrtij.polyanin@gmail.com>
2019-12-30 16:50:27 +0300

    +

---
+++ Client/src/site.template/i18n/en-us.js
@@ -0,0 +1,19 @@
+export default {
+  IndexPage: {
+    users: "Users",
+    password: "Password",
+    version: "Version",
+  },
+  Layout: {
+    madeWithLove: "Made with love"
+  },
+  MaterialInlinePage: {
+  },
+  News2ColPage: {
+    title: "Main page",
+    subTitle: "Example"
+  },
+  SecretPage: {
+    title: "Secret Page"
+  }
+}
---
+++ Client/src/site.template/i18n/ru.js
@@ -0,0 +1,19 @@
+export default {
+  IndexPage: {
+    users: "Пользователи",
+    password: "Пароль",
+    version: "Версия"
+  },
+  Layout: {
+    madeWithLove: "Сделано с Любовью",
+  },
+  MaterialInlinePage: {
+  },
+  News2ColPage: {
+    title: "Главная страница",
+    subTitle: "Пример"
+  },
+  SecretPage: {
+    title: "Секретная страница"
+  }
+}
