abdd331ba297da245469ca34012efd751af59358
Okeanij <dmitrtij.polyanin@gmail.com>
2020-01-21 12:39:59 +0300

    Make User.Link Indexed in db

---
+++ Server/SunEngine.Migrations/Migrations/003-MakeUserLinkIndexed.cs
@@ -0,0 +1,20 @@
+using System;
+using AngleSharp.Dom.Css;
+using FluentMigrator;
+
+namespace SunEngine.Migrations.Migrations
+{
+	[Migration(20200123000000)]
+	public class MakeUserLinkIndexed : Migration
+	{
+		public override void Up()
+		{
+			Alter.Column("Link".s()).OnTable("AspNetUsers".s()).AsString(DbColumnSizes.Users_Link).Indexed().Nullable();
+		}
+
+		public override void Down()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
