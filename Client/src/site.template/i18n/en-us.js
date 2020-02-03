00adff9d0e46312c932370c83ed92b7b9fe40ac8
Okeanij <dmitrtij.polyanin@gmail.com>
2020-02-03 17:29:45 +0300

    Updated Layout

---
+++ Client/src/site.template/i18n/en-us.js
@@ -0,0 +1,18 @@
+export default {
+	IndexPage: {
+		users: "Users",
+		password: "Password",
+		version: "Version"
+	},
+	Layout: {
+		madeWithLove: "Made with love"
+	},
+	MaterialInlinePage: {},
+	News2ColPage: {
+		title: "Main page",
+		subTitle: "Example"
+	},
+	SecretPage: {
+		title: "Secret Page"
+	}
+};
---
+++ Client/src/site.template/i18n/ru.js
@@ -0,0 +1,18 @@
+export default {
+	IndexPage: {
+		users: "Пользователи",
+		password: "Пароль",
+		version: "Версия"
+	},
+	Layout: {
+		madeWithLove: "Сделано с Любовью"
+	},
+	MaterialInlinePage: {},
+	News2ColPage: {
+		title: "Главная страница",
+		subTitle: "Пример"
+	},
+	SecretPage: {
+		title: "Секретная страница"
+	}
+};
