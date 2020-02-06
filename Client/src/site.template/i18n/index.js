7f71674d8f65515ef32794bd2db785570fd8d613
Okeanij <dmitrtij.polyanin@gmail.com>
2020-02-06 20:30:48 +0300

    +

---
+++ Client/src/site.template/i18n/index.js
@@ -0,0 +1,9 @@
+import enUs from "i18n/en-us";
+import ru from "i18n/ru";
+import enUsSite from "./en-us";
+import ruSite from "./ru";
+
+export default {
+	"en-us": { ...enUs, ...enUsSite },
+	ru: { ...ru, ...ruSite }
+};
---
+++ Client/src/site.template/i18n/ru.js
@@ -0,0 +1,9 @@
+export default {
+	PostsAndActivitiesPage: {
+		title: "Главная страница",
+		subTitle: "Пример"
+	},
+	SecretPage: {
+		title: "Секретная страница"
+	}
+};
