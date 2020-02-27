5947c6e44a1610842c1648ec4de4bdb8881e7c10
Okeanij <dmitrtij.polyanin@gmail.com>
2020-02-27 10:43:49 +0300

    Extracting admin module and imports

--- Client/build-index.js
+++ Client/build-index.js
@@ -12,21 +12,20 @@
  *                                                                                      *
  ****************************************************************************************/
 
-const glob = require("glob");
-const fs = require("fs");
+
 
 const dirs = [
-	"api",
-	"classes",
-	"components",
-	"mixins",
-	"shared",
-	"modules",
-	"layouts",
-	"router",
-	"store",
-	"utils",
-	"icons"
+    "api",
+    "classes",
+    "components",
+    "mixins",
+    "shared",
+    "modules",
+    "layouts",
+    "router",
+    "store",
+    "utils",
+    "icons"
 ];
 const adminDirs = ["admin"];
 const excludePaths = ["src/router/index.js"];
@@ -35,92 +34,99 @@ const patternAll = "src/**/*.@(js|vue)";
 const patternAdmin = "src/admin/**/*.@(js|vue)";
 const patternSite = "src/site/**/*.@(js|vue)";
 
-const ind = indexDic();
-const indAdmin = indexDic();
 
-proccess(glob.sync(patternAll), dirs, excludePaths, ind);
+class IndexDic {
+	constructor() {
+		this.dic = {};
+		this.number = 1;
+	}
+
+	addLine(compName, line) {
+		this.dic[compName] = {
+			line: line,
+			number: this.number++
+		};
+	}
+
+	makeText() {
+		let text = "";
+		let arr = [];
+		for (const key in this.dic) arr.push(this.dic[key]);
+
+		arr = arr.sort((a, b) => a.number - b.number);
+
+		for (const obj of arr) text += obj.line + "\n";
+
+		return text;
+	}
+}
+
+const indSun = new IndexDic();
+const indAdmin = new IndexDic();
+
+
+const glob = require("glob");
+const fs = require("fs");
+
+
+proccess(glob.sync(patternAll), dirs, excludePaths, indSun);
 proccess(
-	glob.sync(patternSite),
-	["site"],
-	["src/site/i18n", "src/site/routes.js", "src/admin/adminRoutes.js"],
-	ind
+    glob.sync(patternSite),
+    ["site"],
+    ["src/site/i18n", "src/site/routes.js", "src/admin/adminRoutes.js"],
+    indSun
 );
 proccess(glob.sync(patternAdmin), adminDirs, excludePaths, indAdmin);
 
-ind.addLine("Api", "export Api from 'src/Api.js'");
-ind.addLine("routes", "export routes from 'src/site/routes.js'");
-ind.addLine("store-index", "export * from 'src/store/index'");
-ind.addLine("router", "export {router} from 'src/router/index.js'");
-ind.addLine("App", "export {app} from 'src/App'");
+indSun.addLine("Api", "export Api from 'src/Api.js'");
+indSun.addLine("routes", "export routes from 'src/site/routes.js'");
+indSun.addLine("store-index", "export * from 'src/store/index'");
+indSun.addLine("router", "export {router} from 'src/router/index.js'");
+indSun.addLine("App", "export {app} from 'src/App'");
 
 indAdmin.addLine("adminRoutes", "export adminRoutes from 'src/admin/adminRoutes.js'");
 
 
-fs.writeFile("./src/sun.js", ind.makeText(), function(err) {
-	if (err) return console.log(err);
-	console.log(
-		'\n\x1b[33m☼☼☼   \x1b[32mIndex file generated successfully!\x1b[0m  \x1b[34m"/src/sun.js"   \x1b[33m☼☼☼\x1b[0m\n'
-	);
+fs.writeFile("./src/sun.js", indSun.makeText(), function (err) {
+    if (err) return console.log(err);
+    console.log(
+        '\n\x1b[33m☼☼☼   \x1b[32mIndex file generated successfully!\x1b[0m  \x1b[34m"/src/sun.js"   \x1b[33m☼☼☼\x1b[0m\n'
+    );
 });
 
-fs.writeFile("./src/admin.js", indAdmin.makeText(), function(err) {
-	if (err) return console.log(err);
-	console.log(
-		'\n\x1b[33m☼☼☼   \x1b[32mIndex file generated successfully!\x1b[0m  \x1b[34m"/src/admin.js"   \x1b[33m☼☼☼\x1b[0m\n'
-	);
+fs.writeFile("./src/admin.js", indAdmin.makeText(), function (err) {
+    if (err) return console.log(err);
+    console.log(
+        '\n\x1b[33m☼☼☼   \x1b[32mIndex file generated successfully!\x1b[0m  \x1b[34m"/src/admin.js"   \x1b[33m☼☼☼\x1b[0m\n'
+    );
 });
 
 function proccess(arr, dirs, excludePaths, index) {
-	for (const path of arr) {
-		const name = filePathToComponentName(path, dirs, excludePaths);
-		if (!name) continue;
+    for (const path of arr) {
+        const name = filePathToComponentName(path, dirs, excludePaths);
+        if (!name) continue;
 
-		const fileText = fs.readFileSync(path, "utf8");
+        const fileText = fs.readFileSync(path, "utf8");
 
-		if (/export( )+default/.test(fileText))
-			index.addLine(`${name}`, `export ${name} from '${path}'`);
-		if (/export( )+(?!default)/.test(fileText))
-			index.addLine(`${name}-star`, `export * from '${path}'`);
-	}
+        if (/export( )+default/.test(fileText))
+            index.addLine(`${name}`, `export ${name} from '${path}'`);
+        if (/export( )+(?!default)/.test(fileText))
+            index.addLine(`${name}-star`, `export * from '${path}'`);
+    }
 }
 
 function filePathToComponentName(name, dirs, excludePaths) {
-	if (excludePaths.some(x => name.startsWith(x))) return;
+    if (excludePaths.some(x => name.startsWith(x))) return;
 
-	let arr = name.replace("\\", "/").split("/");
-	if (arr.length <= 1) return;
-	if (!dirs.includes(arr[1])) return;
+    let arr = name.replace("\\", "/").split("/");
+    if (arr.length <= 1) return;
+    if (!dirs.includes(arr[1])) return;
 
-	const fileName = arr[arr.length - 1];
+    const fileName = arr[arr.length - 1];
 
-	arr = fileName.split(".");
-	arr.pop();
+    arr = fileName.split(".");
+    arr.pop();
 
-	return arr.join(".");
+    return arr.join(".");
 }
 
-function indexDic() {
-	return {
-		dic: {},
-		number: 1,
-
-		addLine(compName, line) {
-			this.dic[compName] = {
-				line: line,
-				number: this.number++
-			};
-		},
-
-		makeText() {
-			let text = "";
-			let arr = [];
-			for (const key in this.dic) arr.push(this.dic[key]);
-
-			arr = arr.sort((a, b) => a.number - b.number);
-
-			for (const obj of arr) text += obj.line + "\n";
-
-			return text;
-		}
-	};
-}
